325c33c36bf56db51818257bac5c9c6bce396e7b
Suman Mandal <suman.mandal.develop@gmail.com>
2019-02-06 23:57:39 +0530

    Update form.js

--- public/workspace/component/form/form.js
+++ public/workspace/component/form/form.js
@@ -23,7 +23,7 @@ app.controller('formController', function ($scope, $http) {
             params: data,
             headers: { 'Accept': 'application/json' }
         };
-        $http.get("https://card-validator-application.herokuapp.com//validate", config).then(function (response) {
+        $http.get("https://card-validator-application.herokuapp.com/validate", config).then(function (response) {
             if (response.data.status == "valid") {
                 self.isValid = true;
                 self.notValid = false;
